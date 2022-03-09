7d2804bfe08479325ef261fce6027e32f1fa70c6
joeladia-dev <adiajoel18@gmail.com>
2022-03-09 22:00:34 +0800

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Search your favorite mixtures of cocktails 🍸🍹🍷 using this app that is
+        built using ReactJS!
+      </p>
+      <p>
+        Fun Fact ✔ : The origins of the word cocktail have been debated. The
+        first written mention of cocktail as a beverage appeared in The Farmers
+        Cabinet, 1803 in the United States.
+      </p>
+      <p>
+        a usually iced drink of wine or distilled liquor mixed with flavoring
+        ingredients. something resembling or suggesting such a drink as being a
+        mixture of often diverse elements or ingredients a cocktail of
+        remembered incidents and pure imagination— Charlotte Low a cocktail of
+        herbicides.
+      </p>
+      <p>
+        Warning ⚠ : Cocktails and other alcoholic beverages should be consumed
+        with moderation.
+      </p>
+    </section>
+  );
+};
+
+export default About;
